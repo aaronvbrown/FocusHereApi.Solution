e4a81fe8dda8bcc1b2f220049d7a7ab287fefc0e
Aaron Brown <worldofaaron@hotmail.com>
2024-03-06 18:56:20 -0800

    begin course model building

---
+++ FocusHereApi/Models/Course.cs
@@ -0,0 +1,10 @@
+namespace FocusHereApi.Models
+{
+  public class Course
+  {
+    public int CourseId { get; set; }
+    public string Name { get; set; }
+    public int Teacher { get; set; }
+    public int Period { get; set; }
+  }
+}
